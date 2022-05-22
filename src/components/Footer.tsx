a93573fbc9340f41503782feb567978facbb535d
Nikita Voloboev <nikita.voloboev@gmail.com>
2022-05-23 00:05:01 +0200

    css

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -11,7 +11,7 @@ export default function Footer() {
           padding: 0,
         }}
       ></div>
-      <div class="flex flex-col md:grid md:grid-flow-col">
+      <div class="flex flex-col text-2xl items-center gap-3 md:grid md:grid-flow-col">
         <a href="https://twitter.com/nikitavoloboev">Twitter</a>
         <a href="https://github.com/nikitavoloboev">GitHub</a>
         <a href="https://instagram.com/nikitavoloboev">Instagram</a>
